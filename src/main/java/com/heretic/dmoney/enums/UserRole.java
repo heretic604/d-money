f2e58d472c73c961fde0024ced03dad759e54d33
heretic604 <heretic604@gmail.com>
2023-08-18 11:28:39 +0300

    add enums

---
+++ src/main/java/com/heretic/dmoney/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.heretic.dmoney.enums;
+
+public enum UserRole {
+    VISITOR, ADMIN, SUPER_ADMIN
+}
---
+++ src/main/java/com/heretic/dmoney/enums/UserStatus.java
@@ -0,0 +1,5 @@
+package com.heretic.dmoney.enums;
+
+public enum UserStatus {
+    BLOCKED, ACTIVE
+}
