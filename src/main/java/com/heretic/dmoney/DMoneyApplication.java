6d52f1aecbef3468b4b8b55bd99e9a17870bd505
heretic604 <heretic604@gmail.com>
2023-08-18 10:52:38 +0300

    initial commit

---
+++ src/main/java/com/heretic/dmoney/DMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.heretic.dmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heretic/dmoney/DMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heretic.dmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
