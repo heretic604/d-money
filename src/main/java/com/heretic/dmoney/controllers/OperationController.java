bd5d29aaa9858aeaad5378a024c8977755a2c3e0
heretic604 <heretic604@gmail.com>
2023-08-22 09:14:12 +0300

    add OperationController.java

---
+++ src/main/java/com/heretic/dmoney/controllers/OperationController.java
@@ -0,0 +1,39 @@
+package com.heretic.dmoney.controllers;
+
+import com.heretic.dmoney.dto.requests.OperationRequest;
+import com.heretic.dmoney.dto.responses.OperationResponse;
+import com.heretic.dmoney.services.OperationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class OperationController {
+
+    private final OperationService operationService;
+
+    @PostMapping(value = "operation")
+    public OperationResponse executeOperation(@RequestBody OperationRequest operationRequest) {
+        return operationService.saveOperation(operationRequest);
+    }
+
+    @GetMapping(value = "operation/{id}")
+    public OperationResponse getOperation(@PathVariable UUID id) {
+        return operationService.getOperation(id);
+    }
+
+    @GetMapping(value = "operations")
+    public List<OperationResponse> getOperations() {
+        return operationService.getOperations();
+    }
+
+    @GetMapping(value = "operationsByDate")
+    public List<OperationResponse> getOperations(LocalDate date) {
+        return operationService.getOperations(date);
+    }
+}
