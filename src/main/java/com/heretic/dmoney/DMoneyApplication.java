c9b21a49e76eead52cd34cb1c0fdc871bc4d7e76
heretic604 <heretic604@gmail.com>
2023-08-23 19:56:21 +0300

    update: bug fixes

--- src/main/java/com/heretic/dmoney/DMoneyApplication.java
+++ src/main/java/com/heretic/dmoney/DMoneyApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.heretic.dmoney.*")
-@ComponentScan(basePackages = { "com.heretic.dmoney.*" })
-@EntityScan("com.heretic.dmoney.entities")
-@EnableCon
+//@EnableJpaRepositories("com.heretic.dmoney.*")
+//@ComponentScan(basePackages = { "com.heretic.dmoney.*" })
+//@EntityScan("com.heretic.dmoney.entities")
 @SpringBootApplication
 public class DMoneyApplication {
     public static void main(String[] args) {
